275a923d37e493cff6ffb2de9afea1a1829f11c8
shifat97 <shifatbinreza2@gmail.com>
2024-04-01 20:34:14 +0600

    fixed some functionality in convertToMeter() function

--- script.js
+++ script.js
@@ -7,18 +7,12 @@ function multiply(input1ValueToInt, input2ValueToInt) {
     return input1ValueToInt * input2ValueToInt;
 }
 
-// This function convert the centimeter to meter
-function convertToMeter(area, serial) {
-    let getIndex = areaCalculationDiv.lastChild.firstChild.nextSibling.firstChild.nextSibling.firstChild.nextSibling;
-    let getIndexText = getIndex.innerText;
-    let index = getIndexText.split(".")[0];
-
-    // Change the innerText of paragraph if serial is matched to index
-    if(Number(index) === Number(serial)) {
-        let getCentimeter = getIndex.parentNode.nextSibling.nextSibling;
-        let centimeter = area * 0.01;
-        getCentimeter.innerHTML = `${centimeter.toFixed(2)}m<sup>2</sup>`;
-    }
+// This function converts the centimeter to meter
+function convertToMeter(button) {
+    const getCentimeter = button.previousSibling.previousSibling;
+    const getValue = getCentimeter.innerText.split('c')[0];
+    const centimeterToMeter = parseFloat(getValue) * 0.01;
+    getCentimeter.innerHTML = `${centimeterToMeter.toFixed(2)}m<sub>2</sub>`
 }
 
 // This function adds the area and title to right sidebar
@@ -32,7 +26,7 @@ function areaCalculation(area, title) {
                 <p>${title}</p>
             </div>
             <p>${area}cm<sup>2</sup></p>
-            <button class="text-white bg-[#1090D8] rounded-[4px] px-[10px] py-[7px]" onclick="convertToMeter(${area}, ${serial})">Convert to m<sup>2</sup></button>
+            <button class="text-white bg-[#1090D8] rounded-[4px] px-[10px] py-[7px]" onclick="convertToMeter(this)">Convert to m<sup>2</sup></button>
         </div>
     `
     // Each time serial will increase by 1
@@ -41,63 +35,61 @@ function areaCalculation(area, title) {
     areaCalculationDiv.appendChild(parentDiv);
 }
 
-// This loop iterate through all the calculate button
-for (let cal of calculate) {
+function getValue(cal) {
     // Getting the first input element
     const input1 = cal.previousSibling.previousSibling.firstChild.nextSibling.firstChild.nextSibling;
     // Getting the second input element
     const input2 = cal.previousSibling.previousSibling.lastChild.previousSibling.previousSibling.previousSibling.firstChild.nextSibling;
+    // Getting input fields value
+    let input1Value = input1.value;
+    let input2Value = input2.value;
 
+    return [input1Value, input2Value];
+}
+
+// Calculation area depend on title
+// Formula will be applied depending on the title
+function calculateArea(title, input1ValueToInt, input2ValueToInt) {
+    let mul = multiply(input1ValueToInt, input2ValueToInt);
+    let area;
+    if(title.innerText.toLowerCase() === "triangle") {
+        area = 0.5 * mul;
+    } else if(title.innerText.toLowerCase() === "rectangle") {
+        area = mul;
+    } else if(title.innerText.toLowerCase() === "parallelogram") {
+        area = mul;
+    } else if(title.innerText.toLowerCase() === "rhombus") {
+        area = 0.5 * mul;
+    } else if(title.innerText.toLowerCase() === "pentagon") {
+        area = 0.5 * mul;
+    } else if(title.innerText.toLowerCase() === "ellipse") {
+        area = 3.1416 * mul;
+    }
+    return area;
+}
+
+// This loop iterate through all the calculate button
+for (let cal of calculate) {
     // Click handler for each loop
     cal.addEventListener('click', function () {
+        let value = getValue(cal);
         const title = cal.parentNode.firstChild.nextSibling.nextSibling.nextSibling;
 
-        // Getting input fields value
-        let input1Value = input1.value;
-        let input2Value = input2.value;
-
         // Checking if input value is null
-        let isEmptyInput1 = input1Value === "";
-        let isEmptyInput2 = input2Value === "";
-
-        if(isEmptyInput1 || isEmptyInput2) {
+        if(value[0] === "" || value[1] === "") {
             alert("Please enter values...");
         } else {
             // Checking if input is string or not a number
-            if (isNaN(input1Value) && isNaN(input2Value)) {
+            if (isNaN(value[0]) && isNaN(value[1])) {
                 alert('Enter numbers only');
             } else {
                 // Converting input value to integer
-                const input1ValueToInt = Number(input1Value);
-                const input2ValueToInt = Number(input2Value);
-
-                // Call the multiply() function for multiply 2 value
-                let mul = multiply(input1ValueToInt, input2ValueToInt);
-
-                // Calculation area depend on title
-                // Formula will be applied depending on the title
-                let area;
-                if(title.innerText.toLowerCase() === "triangle") {
-                    area = 0.5 * mul;
-                } else if(title.innerText.toLowerCase() === "rectangle") {
-                    area = mul;
-                } else if(title.innerText.toLowerCase() === "parallelogram") {
-                    area = mul;
-                } else if(title.innerText.toLowerCase() === "rhombus") {
-                    area = 0.5 * mul;
-                } else if(title.innerText.toLowerCase() === "pentagon") {
-                    area = 0.5 * mul;
-                } else if(title.innerText.toLowerCase() === "ellipse") {
-                    area = 3.1416 * mul;
-                }
-
+                const input1ValueToInt = Number(value[0]);
+                const input2ValueToInt = Number(value[1]);
+                let area = calculateArea(title, input1ValueToInt, input2ValueToInt)
                 // Add area and title to area calculation div
                 areaCalculation(area.toFixed(2), title.innerText);
             }
         }
-
-        // Clear the input field
-        input1.value = "";
-        input2.value = "";
     })
 }
